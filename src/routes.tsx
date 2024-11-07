1e480170a824725a16ac1088109ecbf3f5608940
Afonso Mateus <afonsomateus.dev@gmail.com>
2024-11-07 19:08:59 -0300

    :wrench: | config: adiciona rota da home e cartao fidelidade no arquivo de rotas

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
 import { Login } from "./pages/Login";
+import { Home } from "./pages/Home";
+import { FidelityCard } from "./pages/FidelityCard";
 
 export const router = createBrowserRouter([
   {
@@ -10,7 +12,15 @@ export const router = createBrowserRouter([
       {
         path: "/",
         element: <Login />
-      }
+      },
     ]
-  }
+  },
+  {
+    path: "/home",
+    element: <Home />
+  },
+  {
+    path: "/fidelity-card",
+    element: <FidelityCard />
+  },
 ]);
